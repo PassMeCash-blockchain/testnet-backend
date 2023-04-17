18652dd5f970c7e8eba1c5bd8df393e46d4e8c24
Scofield Whitefire <gabrielprecioux@gmail.com>
2023-04-17 03:13:36 -0400

    Delete cdn directory

--- cdn/store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(CSSFile)
-admin.site.register(PdfFile)
-admin.site.register(ImageFile)
-admin.site.register(JSFile)
