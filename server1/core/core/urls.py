e360ae1ed3356afe29ba7d695ac87d264495503c
Scofield Whitefire <gabrielprecioux@gmail.com>
2023-04-17 03:11:45 -0400

    Delete server1 directory

--- server1/core/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('_/', admin.site.urls),
-    
-    # endpoints
-    path('endpoint/v1/', include('endpoints.urls')),
-]
